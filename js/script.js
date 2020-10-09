824d73ef22a9b80d67759f47805c9994cc8b86e0
kihlaj <65562071+kihlaj@users.noreply.github.com>
2020-10-09 14:25:44 +0300

    js continuted

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const CreateCats = (() => {
   function makeCat(
     make,
     country,
-    img = 'img/cat-default.jpeg',
+    img = 'img/cat-default.jpg',
     special = true,
     breed = 'new breed',
     price = 99,
@@ -56,17 +56,16 @@ const CreateCats = (() => {
   }
   // produce cats
   function produceCats() {
-    makeCat('persian', 'iran')
-    makeCat('Bobcat', 'Canada', 'img/cat-wild-1.jpge', true)
-    makeCat('Bombay', 'World Wide', 'img/cat-wild-2.jpge')
-    makeCat('Black Tiger', 'Asia', 'img/cat-wild-3.jpge')
-    makeCat('lion', 'Africa', 'img/cat-wild-4.jpge', false)
-    makeCat('Bombay', 'World Wide', 'img/cat-wild-5.jpge', false)
-    makeCat('Bombay', 'World Wide', 'img/cat-cute-1.jpge')
-    makeCat('Bombay', 'World Wide', 'img/cat-cute-2.jpge', false)
-    makeCat('Bombay', 'World Wide', 'img/cat-cute-3.jpge', false)
-    makeCat('Bombay', 'World Wide', 'img/cat-cute-4.jpge', false)
-    makeCat('Bombay', 'World Wide', 'img/cat-cute-5.jpge', false)
+    makeCat('persian', 'iran', 'img/cat-wild-1.jpg', true)
+    makeCat('Bobcat', 'America', 'img/cat-wild-2.jpg')
+    makeCat('Black Tiger', 'Asia', 'img/cat-wild-3.jpg')
+    makeCat('lion', 'Africa', 'img/cat-wild-4.jpg', false)
+    makeCat('Wildy', 'World Wide', 'img/cat-wild-5.jpg', false)
+    makeCat('Bombay', 'World Wide', 'img/cat-cute-1.jpg')
+    makeCat('Bombay', 'World Wide', 'img/cat-cute-2.jpg', false)
+    makeCat('Bombay', 'World Wide', 'img/cat-cute-3.jpg', false)
+    makeCat('Bombay', 'World Wide', 'img/cat-cute-4.jpg', false)
+    makeCat('Bombay', 'World Wide', 'img/cat-cute-5.jpg', false)
   }
   produceCats()
 
@@ -90,6 +89,85 @@ const DisplaySpecialCats = ((CreateCats) => {
 
     let data = ''
 
-    specialCats.forEach(item)
+    specialCats.forEach((item) => {
+      data += `          <!-- single item -->
+          <div class="featured-item my-3 d-flex p-2 text-capitalize align-items-baseline flex-wrap">
+            <span data-img="${item.img}" class="featured-icon mr-2"><i class="fas fa-cat"></i></span>
+            <h5 class="font-weight-bold mx-1">${item.make}</h5>
+            <h5 class="mx-1">${item.breed}</h5>
+          </div>
+          <!-- end of single item -->`
+    })
+    info.innerHTML = data
+  })
+  // changing picture
+  info.addEventListener('click', (event) => {
+    if (event.target.parentElement.classList.contains('featured-icon')) {
+      const img = event.target.parentElement.dataset.img
+      document.querySelector('.featured-photo').src = img
+    }
   })
 })(CreateCats)
+
+const DisplayCats = ((CreateCats) => {
+  // all cats
+  const cats = CreateCats.cats
+  // cat container
+  const inventory = document.querySelector('.inventory-container')
+
+  //content loaded
+  document.addEventListener('DOMContentLoaded', () => {
+    inventory.innerHTML = ''
+    let output = ''
+    cats.forEach((cat) => {
+      output += `        <!-- single cat -->
+        <div class="col-10 mx-auto my-3 col-md-6 col-lg-4 single-cat ${cat.country}">
+          <div class="card cat-card">
+            <img src="${cat.img}" class="card-img-top cat-img" alt="">
+            <!-- card body -->
+            <div class="card-body">
+              <div class="cat-info d-flex justify-content-between">
+                <!-- first flex child -->
+                <div class="cat-text text-uppercase">
+                  <h6 class="font-weight-bold">${cat.make}</h6>
+                  <h6>${cat.make}</h6>
+                </div>
+                <!-- second flex child -->
+                <h5 class="cat-value align-self-center py-2 px-3">
+                  $<span class="cat-price">${cat.price}</span>
+                </h5>
+              </div>
+            </div>
+            <!-- end of card -->
+            <div class="card-footer text-capitalize d-flex justify-content-between">
+              <p><span><i class="fas fa-cat"></i>${cat.breed}</span></p>
+              <p><span><i class="fas fa-stopwatch"></i>${cat.age}</span></p>
+              <p><span><i class="fas fa-user-friends"></i>${cat.personality}</span></p>
+            </div>
+          </div>
+        </div>
+        <!-- end of single cat -->`
+    })
+    inventory.innerHTML = output
+  })
+})(CreateCats)
+// filter cats
+const FilterCats = (() => {
+  const filter = document.querySelectorAll('.filter-btn')
+  filter.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      const value = event.target.dataset.filter
+      const singleCat = document.querySelectorAll('.single-cat')
+      singleCat.forEach((cat) => {
+        if (value === 'all') {
+          cat.style.display = 'block'
+        } else {
+          !cat.classList.contains(value)
+            ? (cat.style.display = 'none')
+            : (cat.style.display = 'block ')
+        }
+      })
+    })
+  })
+})()
+// show modal
